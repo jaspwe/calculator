23015730271cbc6a99923a90362911c580406dd3
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-04-01 14:18:29 +0100

    created operate function
    this takes an operator and two values and calls
    a mathamatical funtion.

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ function divide(origin, number){
     return origin / number;
 }
 
+function operate(operator, num1, num2){
+    if (operator == '+'){
+        add(num1, num2);
+    } else if (operator == '-'){
+        subtract(num1, num2)
+    } else if (operator == '*'){
+        multiply(num1, num2)
+    } else if (operator == '/'){
+        divide(num1, num2)
+    }
+}
+
 let num1 = 0
 let operator = '+'
 let num2 = 0
