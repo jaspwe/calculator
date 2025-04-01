577e3d40472a9564773ca94dddd5043cea7ecae7
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-04-01 14:06:46 +0100

    created functions for basic math operators

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(origin, number){
+    return origin + number;
+}
+
+function subtract(origin, number){
+    return origin - number;
+}
+
+function multiply(origin, number){
+    return origin * number;
+}
+
+function divide(origin, number){
+    return origin / number;
+}
