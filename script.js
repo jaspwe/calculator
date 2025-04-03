e9a834c98f8dc25cd24b55d3387a7c279130669d
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-04-03 11:00:40 +0100

    Finished Calculator
    to improve, i could add decimals and a delete
    button.

--- script.js
+++ script.js
@@ -16,19 +16,109 @@ function divide(origin, number){
 
 function operate(operator, num1, num2){
     if (operator == '+'){
-        add(num1, num2);
+        return add(num1, num2);
     } else if (operator == '-'){
-        subtract(num1, num2)
+        return subtract(num1, num2);
     } else if (operator == '*'){
-        multiply(num1, num2)
+        return multiply(num1, num2);
     } else if (operator == '/'){
-        divide(num1, num2)
+        return divide(num1, num2);
     }
 }
 
-let num1 = 0
-let operator = '+'
-let num2 = 0
+let num1 = 0;
+let operator = '';
+let num2 = 0;
 
-const display = document.querySelector(".display")
-const buttons = document.querySelectorAll("button")
+const display = document.querySelector(".display");
+const numberPad = document.querySelector(".number-pad");
+
+//number buttons
+for (i = 1; i < 10; i++){
+    const button = document.createElement("button")
+    button.textContent = i;
+    button.id = i;
+    button.addEventListener("click", function(){
+        if (display.textContent.length < 15){
+            display.textContent = display.textContent + button.id;
+        }
+    })
+    numberPad.appendChild(button);
+}
+const button = document.createElement("button")
+    button.textContent = 0;
+    button.id = 0;
+    button.addEventListener("click", function(){
+        if (display.textContent.length < 15){
+            display.textContent = display.textContent + button.id;
+        }
+    })
+    numberPad.appendChild(button);
+
+//clear button
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", function(){
+    display.textContent = "";
+    num1 = 0;
+    num2 = 0;
+})
+
+//add
+const addition = document.querySelector("#plus");
+addition.addEventListener("click", function(){
+    if (display.textContent !== "") {
+        num1 = operator ? operate(operator, num1, Number(display.textContent)) : Number(display.textContent);
+        display.textContent = "";
+        operator = '+';
+    }
+});
+
+
+//subtract
+const subtraction = document.querySelector("#minus");
+subtraction.addEventListener("click", function(){
+    if (display.textContent !== "") {
+        num1 = operator ? operate(operator, num1, Number(display.textContent)) : Number(display.textContent);
+        display.textContent = "";
+        operator = '-';
+    }
+});
+
+//multiply
+const multiplication = document.querySelector("#multiply");
+multiplication.addEventListener("click", function(){
+    if (display.textContent !== "") {
+        num1 = operator ? operate(operator, num1, Number(display.textContent)) : Number(display.textContent);
+        display.textContent = "";
+        operator = '*';
+    }
+});
+
+//divide
+const division = document.querySelector("#divide");
+division.addEventListener("click", function(){
+    if (display.textContent !== "") {
+        num1 = operator ? operate(operator, num1, Number(display.textContent)) : Number(display.textContent);
+        display.textContent = "";
+        operator = '/';
+    }
+});
+
+//equals
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", function(){
+    if (operator && display.textContent !== "" && !isNaN(Number(display.textContent))) {
+        num1 = operate(operator, num1, Number(display.textContent));
+        if (num1.toString().length > 14){
+            if (num1 % 1 != 0){
+                display.textContent = num1.toPrecision(15);
+            } else{
+                display.textContent = "NUM TOO LONG"
+                console.log("Answer: " + num1);
+            }
+        } else {
+            display.textContent = num1;
+        }
+        operator = "";
+    }
+});
